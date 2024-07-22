d50906e3ffd61db8d36c375bee2624c9104fac53
ZinedineZethro <zethrozinedine@gmail.com>
2024-07-22 17:27:24 +0700

    updated Dashboard Controller

--- src/digicob/app/Http/Controllers/CompanyController.php
+++ src/digicob/app/Http/Controllers/CompanyController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
+use App\Models\UserCompany;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index($companyId)
+    {
+        $user = auth()->user();
+
+        $company = Company::find($companyId);
+
+        $userCompany = UserCompany::where('companyId', $companyId)
+            ->where('userId', $user->userId)
+            ->first();
+
+
+    }
 }
--- src/digicob/app/Http/Controllers/DashboardController.php
+++ src/digicob/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
+use App\Models\DomainCompany;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use app\Models\UserCompany;
@@ -10,28 +11,25 @@ use app\Models\UserCompany;
 
 class DashboardController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $user = auth()->user();
 
         $userCompanyList = $user->userCompanies()->with('company')->get();
 
-        return view('dashboard', compact('userCompanyList'));
+        $companyIds = $userCompanyList->pluck('companyId');
+
+        $domainCompanyList = DomainCompany::where('userId', $user->userId)
+            ->where('companyId', $companyIds)
+            ->with('domain')
+            ->get();
+
+        return view('dashboard', compact('userCompanyList', 'domainCompanyList'));
     }
 
     public function addNewCompany(Request $request)
@@ -57,12 +55,20 @@ class DashboardController extends Controller
             $company->save();
         }
 
+        $userCompany = UserCompany::where('userId', $user->userId)
+                ->where('companyId', $company->companyId)
+                ->first();
+
+        if($userCompany){
+            return response()->json(['message' => 'Company already exists.'], 409);
+        }
+
         $userCompany = new UserCompany();
         $userCompany->userId = $user->userId;
         $userCompany->companyId = $company->companyId;
         $userCompany->userCompanyScore = 0.0;
         $userCompany->save();
 
-        return response()->json(['message' => 'Company has been added successfully.'], 200);
+        return response()->json(['message' => 'Company has been added successfully.'], 201);
     }
 }
